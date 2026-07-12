959ddd612e672d489118daf8edf3cfa4f9893655
LE0N9RE <leondre0nosworthy@gmail.com>
2026-07-12 16:28:28 -0400

    save 1 for reboot

--- nixos/disko.nix
+++ nixos/disko.nix
@@ -1,63 +1,108 @@
+{ ... }:
 {
+  fileSystems."/nix".neededForBoot = true;
+  fileSystems."/persistent".neededForBoot = true;
+
   disko.devices = {
     nodev."/" = {
       fsType = "tmpfs";
+
       mountOptions = [
         "size=25%"
         "mode=755"
       ];
     };
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "1G"; # Matches your current 1GB boot partition
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+
+    disk.main = {
+      type = "disk";
+
+      # Stable path for your Patriot P300 1 TB NVMe drive.
+      device =
+        "/dev/disk/by-id/nvme-Patriot_M.2_P300_1024GB_P300WCBA25040111438";
+
+      content = {
+        type = "gpt";
+
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "EFI";
+            start = "1M";
+            size = "1G";
+            type = "EF00";
+
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+
+              mountOptions = [
+                "umask=0077"
+              ];
             };
-            swap = {
-              size = "8.8G"; # Matches your current swap partition size
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true;
-              };
+          };
+
+          swap = {
+            priority = 2;
+            name = "swap";
+            size = "8.8G";
+
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+              resumeDevice = true;
             };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  "/persistent" = {
-                    mountpoint = "/persistent";
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                };
-              };
+          };
+
+          nix = {
+            priority = 3;
+            name = "nix";
+            size = "300G";
+
+            content = {
+              type = "filesystem";
+              format = "f2fs";
+              mountpoint = "/nix";
+
+              extraArgs = [
+                "-f"
+                "-l"
+                "nix"
+              ];
+
+              mountOptions = [
+                "noatime"
+                "lazytime"
+                "discard"
+              ];
+            };
+          };
+
+          persistent = {
+            priority = 4;
+            name = "persistent";
+            size = "100%";
+
+            content = {
+              type = "filesystem";
+              format = "f2fs";
+              mountpoint = "/persistent";
+
+              extraArgs = [
+                "-f"
+                "-l"
+                "persistent"
+              ];
+
+              mountOptions = [
+                "noatime"
+                "lazytime"
+                "discard"
+              ];
             };
           };
         };
       };
     };
   };
-
-  fileSystems."/nix".neededForBoot = true;
-  fileSystems."/persistent".neededForBoot = true;
 }
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,41 +1,46 @@
 {
   description = "My NixOS flake configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     mangowm.url = "github:mangowm/mango";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    preservation.url = "github:Wandmalfarbe/preservation";
+
+    preservation.url = "github:nix-community/preservation";
   };
+
   outputs =
-    { self, nixpkgs,disko, preservation, ... }@inputs:
+    { nixpkgs, ... }@inputs:
     {
-      nixosConfigurations.a3n =
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+      nixosConfigurations.a3n = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            inputs.mangowm.nixosModules.mango
-            disko.nixosModules.disko
-            preservation.nixosModules.preservation
-
-            ######################################
-            ./configuration.nix
-            ./hardware-configuration.nix
-
-            ./modules/hardware.nix
-            ./modules/system.nix
-            ./modules/user.nix
-            ./modules/utils.nix
-            ./modules/packages.nix
-            ./modules/mangowm.nix
-            ./disko.nix
-            ./preservation.nix
-          ];
+        specialArgs = {
+          inherit inputs;
         };
+
+        modules = [
+          inputs.mangowm.nixosModules.mango
+          inputs.disko.nixosModules.disko
+          inputs.preservation.nixosModules.preservation
+
+          ./configuration.nix
+          ./hardware-configuration.nix
+
+          ./modules/hardware.nix
+          ./modules/system.nix
+          ./modules/user.nix
+          ./modules/utils.nix
+          ./modules/packages.nix
+          ./modules/mangowm.nix
+
+          ./disko.nix
+          ./preservation.nix
+        ];
+      };
     };
 }
