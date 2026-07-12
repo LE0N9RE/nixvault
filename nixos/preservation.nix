76a1e9fe056b015f02c652c73894ac879dab978d
LE0N9RE <leondre0nosworthy@gmail.com>
2026-07-12 16:56:05 -0400

    another change smh

--- nixos/preservation.nix
+++ nixos/preservation.nix
@@ -1,10 +1,9 @@
-{ config, ... }: {
-  # Turn on preservation
+{ config, ... }:
+
+{
   preservation.enable = true;
 
-  # Tell the system to save these things to persistent storage area
-  preservation.preserve."/persistent" = {
-    # System settings that must stay across reboots
+  preservation.preserveAt."/persistent" = {
     files = [
       "/etc/machine-id"
     ];
@@ -15,22 +14,21 @@
       "/var/lib/NetworkManager"
     ];
 
-    # = user folders will not be delated
     users.a3n = {
       directories = [
-        "dotfiles"       # GitHub repo folder
-        ".config"        # app settings (Discord, themes, etc.)
-        "Projects"       # coding/development projects
-        "Programs"       # custom software/binaries
-        "Documents"      # Personal documents
-        "Pictures"       # photos/wallpapers
-        "Videos/Saved"   # special folder for videos to keep permanently
+        "dotfiles"
+        ".config"
+        "Projects"
+        "Programs"
+        "Documents"
+        "Pictures"
+        "Videos/Saved"
       ];
 
       files = [
-        ".bash_history"     # Keeps terminal command history
-        ".gitconfig"        # Keeps GitHub identity
-        ".git-credentials"  # Keeps GitHub login active
+        ".bash_history"
+        ".gitconfig"
+        ".git-credentials"
       ];
     };
   };
